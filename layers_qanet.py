096eb2810c1161aa803963945f576eefaee6bc4f
Akshita Agarwal <agarwalakshita31@gmail.com>
2022-03-06 00:43:33 +0000

    move the previous positional encoding to archive

--- layers_qanet.py
+++ layers_qanet.py
@@ -221,33 +221,6 @@ def get_timing_signal(length, channels,
     signal = m(signal)
     signal = signal.view(1, length, channels)
     return signal
-
-class PositionalEncoder(nn.Module):
-    #Reference: https://github.com/tatp22/multidim-positional-encoding/blob/master/positional_encodings/positional_encodings.py
-    def __init__(self, in_channels):
-        super(PositionalEncoder, self).__init__()
-
-        if in_channels%2 == 0:
-            self.channels = in_channels
-        else:
-            self.channels = in_channels + 1
-
-        self.frequency_factor = 1.0 / (10000 ** (torch.arange(0, self.channels, 2).float() / self.channels))
-
-    def forward(self, tensor):
-
-        batch_size, x, orig_ch = tensor.shape
-        # print("positional encoding orig shape", tensor.shape)
-        pos_x = torch.arange(x, device=tensor.device).type(self.frequency_factor.type())
-        sin_inp_x = torch.einsum("i,j->ij", pos_x, self.frequency_factor)
-        # print("sin_inp_x apply sincos", sin_inp_x.shape)
-        emb_x = torch.cat((sin_inp_x.sin(), sin_inp_x.cos()), dim=-1)
-        #print("embx apply sincos", emb_x.shape)
-        emb = torch.zeros((x, self.channels), device=tensor.device).type(tensor.type())
-        #print("emb zeros", emb.shape)
-        emb[:, : self.channels] = emb_x
-        #print("output", emb[None, :, :orig_ch].repeat(batch_size, 1, 1).shape)
-        return emb[None, :, :orig_ch].repeat(batch_size, 1, 1)
       
 class QANetEmbedding(nn.Module):
     """Combines the Word and Character embedding and then applies a transformation and highway network.
